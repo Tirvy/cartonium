2f2b952bbc148e141084352a0e179fc287ad6877
Ernest <8tirvy@gmail.com>
2024-02-25 16:50:54 +0400

    fast action component

---
+++ components/fastAction.vue
@@ -0,0 +1,20 @@
+
+// place at the root level of a page template
+
+<template>
+    <v-layout-item model-value class="text-end pointer-events-none" position="bottom" size="88">
+        <div class="ma-4 pointer-events-initial">
+            <slot></slot>
+        </div>
+    </v-layout-item>
+</template>
+
+<style scoped>
+.pointer-events-none {
+    pointer-events: none;
+}
+
+.pointer-events-initial {
+    pointer-events: initial;
+}
+</style>
--- pages/clubs/[clubname]/information/public.vue
+++ pages/clubs/[clubname]/information/public.vue
@@ -7,11 +7,9 @@
         </v-container>
     </v-main>
 
-    <v-layout-item model-value class="text-end pointer-events-none" position="bottom" size="88">
-        <div class="ma-4 pointer-events-initial">
-            <v-btn icon="mdi-pencil-outline" size="large" elevation="8" :to="editPath" />
-        </div>
-    </v-layout-item>
+    <fast-action>
+        <v-btn icon="mdi-pencil-outline" size="large" elevation="8" :to="editPath" />
+    </fast-action>
 </template>
 
 <script setup>
@@ -27,16 +25,4 @@ async function getInfo() {
     infoData.value = data.text_html;
 }
 getInfo();
-
-
 </script>
-
-<style scoped>
-.pointer-events-none {
-    pointer-events: none;
-}
-
-.pointer-events-initial {
-    pointer-events: initial;
-}
-</style>ƒ
