2df32822eb15963a25d75d2b1ea268942a7dd509
Ernest <8tirvy@gmail.com>
2024-04-10 20:54:54 +0400

    login with email

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@ definePageMeta({
 const user = useSupabaseUser()
 const { auth } = useSupabaseClient()
 
+
 watchEffect(() => {
   if (user.value) {
     navigateTo('/clubs/emarena/collection')
@@ -18,6 +19,29 @@ async function signInWithProvider(provider: string) {
     provider: provider as Provider,
   })
 }
+
+const loginEmail = ref('');
+const loginPassword = ref('');
+async function signInWithEmail() {
+  const { data, error } = await auth.signInWithPassword({
+    email: loginEmail.value,
+    password: loginPassword.value,
+  })
+}
+
+const signupEmail = ref('');
+const signupPassword = ref('');
+async function signUpNewUser() {
+  const { data, error } = await auth.signUp({
+    email: signupEmail.value,
+    password: signupPassword.value,
+    options: {
+      emailRedirectTo: 'http://192.168.100.5:3000/clubs/emarena/add-games',
+    },
+  })
+}
+
+
 </script>
 
 <template>
@@ -26,21 +50,34 @@ async function signInWithProvider(provider: string) {
       <v-col class="d-flex justify-center">
         <v-card>
           <v-card-title>
-            Sign in to your account
+            login with email
           </v-card-title>
-          <div>
-            <v-divider />
-
-            <div class="text-center">
-              connect with
-            </div>
-
-          </div>
+          <v-card-actions>
+            <v-form class="w-100">
+              <v-text-field v-model="loginEmail" label="email"></v-text-field>
+              <v-text-field v-model="loginPassword" label="email"></v-text-field>
+              <v-btn @click="signInWithEmail">login</v-btn>
+            </v-form>
+          </v-card-actions>
+          <v-card-title>
+            login with providers
+          </v-card-title>
           <v-card-actions>
             <v-btn icon="mdi-github" @click="signInWithProvider('github')"></v-btn>
             <v-btn icon="mdi-google" @click="signInWithProvider('google')"></v-btn>
             <v-btn icon="mdi-facebook" @click="signInWithProvider('facebook')"></v-btn>
             <v-btn icon="mdi-discord" @click="signInWithProvider('discord')">d</v-btn>
+          </v-card-actions>
+          <v-divider />
+          <v-card-title>
+            signup with email
+          </v-card-title>
+          <v-card-actions>
+            <v-form class="w-100">
+              <v-text-field v-model="signupEmail" label="email"></v-text-field>
+              <v-text-field v-model="signupPassword" label="email"></v-text-field>
+              <v-btn @click="signUpNewUser">sign up</v-btn>
+            </v-form>
           </v-card-actions>
         </v-card>
       </v-col>
