d0968a8475e6212f93cffef32583e8edb2b7e247
Ernest <8tirvy@gmail.com>
2024-05-20 12:30:43 +0400

    dev-ui-kit

---
+++ pages/dev/ui-kit.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-col>
+
+          <table>
+            <tr>
+              <td v-for="variant in buttonVaritants">
+                {{ variant }}
+              </td>
+
+            </tr>
+            <tr>
+              <td v-for="variant in buttonVaritants">
+                <v-btn :variant="variant">котики</v-btn>
+              </td>
+            </tr>
+          </table>
+        </v-col>
+      </v-row>
+
+    </v-container>
+  </v-main>
+</template>
+
+<script lang="ts" setup>
+const buttonVaritants = ['elevated', 'flat', 'tonal', 'outlined', 'text', 'plain'] as const;
+</script>
+
+<style scoped></style>
