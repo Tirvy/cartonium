75ba6844cc8fd66a5b1ab475cc33d99096d280ba
Ernest <8tirvy@gmail.com>
2024-10-28 15:58:14 +0400

    metaupdates

--- pages/auth/telegram-callback-page.vue
+++ pages/auth/telegram-callback-page.vue
@@ -21,7 +21,7 @@ onMounted(async () => {
 
   const route = useRoute();
   const query = route.query;
-  console.log(query)
+  const tgData = query as unknown as TelegramLoginPayload;
 
   const credsRes = await $fetch<{ login: string, password: string }>('/api/common/get-creds', { query });
   if (!credsRes) {
@@ -36,10 +36,18 @@ onMounted(async () => {
       email,
       password,
     })
-    if (signInRes.error) {
+    if (!signInRes.error && signInRes.data) {
+      await supabase.auth.updateUser({
+        data: getMetadataObject(tgData),
+      })
+    }
+    if (signInRes.error && !signInRes.data) {
       const signUpRes = await supabase.auth.signUp({
         email,
         password,
+        options: {
+          data: getMetadataObject(tgData),
+        }
       })
     }
     navigateTo(query.next as string || '/', {
@@ -47,6 +55,19 @@ onMounted(async () => {
     })
   }
 });
+
+
+function getMetadataObject(tgData: TelegramLoginPayload) {
+  return {
+    telegram_id: tgData.id,
+    full_name: [tgData.first_name, tgData.last_name].filter(Boolean).join(' '),
+    first_name: tgData.first_name,
+    avatar_url: tgData.photo_url,
+    picture: tgData.photo_url,
+    telegram_username: tgData.username,
+    is_imaginery: true,
+  }
+}
 
 </script>
 
