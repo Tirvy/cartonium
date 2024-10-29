5a2800fdc23c8f04fc7c45191dede29686ca76e3
dmitrii <d.astretsov@gmail.com>
2024-10-29 19:02:28 +0300

    add api method for posting pics to bucket, add method trigger while adding new game

---
+++ server/api/supabase/gamebox-picture.post.ts
@@ -0,0 +1,23 @@
+import { serverSupabaseClient } from '#supabase/server'
+import type { Database } from '~/types/supabase.js'
+import { blobToFile } from '@/utils/helpers';
+
+export default defineEventHandler(async (event) => {
+  const client = await serverSupabaseClient<Database>(event);
+  const body = await readBody(event);
+  const gamebox = body.gamebox;
+
+  const resp: any = await $fetch(gamebox.photoUrl);
+  const image = blobToFile(resp, gamebox.id);
+  const { data, error } = await client.storage.from('gamebox-pics').upload(
+    gamebox.id.toString(), 
+    image, 
+    { upsert: true },
+  );
+
+  if (error) {
+    throw createError({ statusMessage: error.message })
+  } 
+  
+  return data;
+});
