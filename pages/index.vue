b32bab101d55d0f11723a9b51752f43e47dfa751
Ernest <8tirvy@gmail.com>
2025-11-03 13:37:26 +0400

    landing wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,18 @@
 <template>
-  <v-container>
-    <v-container fluid class="fill-height">
+  <v-main>
+    <v-container fluid class="bg-purple-lighten-5 banner-container align-content-center">
+      <v-spacer class="my-20"></v-spacer>
       <v-row>
         <v-col class="d-flex justify-center">
           <v-container>
             <v-row>
-              <v-col class="d-flex justify-center">
-                <h1>{{ $t('landing.welcome') }}</h1>
+              <v-col class="d-flex justify-center text-center text-h4">
+                {{ $t('landing.welcome') }}
               </v-col>
             </v-row>
             <v-row class="justify-space-around">
               <v-col class="d-flex justify-center">
-                <v-card>
+                <v-card @click="scrollTo('#players')">
                   <v-card-title>
                     {{ $t('landing.welcome_players') }}
                   </v-card-title>
@@ -21,7 +22,7 @@
                 </v-card>
               </v-col>
               <v-col class="d-flex justify-center">
-                <v-card>
+                <v-card @click="scrollTo('#clubs')">
                   <v-card-title>
                     {{ $t('landing.welcome_clubs') }}
                   </v-card-title>
@@ -34,99 +35,115 @@
           </v-container>
         </v-col>
       </v-row>
+      <v-spacer class="my-10"></v-spacer>
     </v-container>
-    <v-spacer class="my-10"></v-spacer>
-    <v-container>
-      <v-card>
-        <v-card-title>
-          {{ $t('landing.players_title') }}
-        </v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col cols="8">
-              <v-card>
-                <v-card-title>
-                  {{ $t('landing.players_where_title') }}
-                </v-card-title>
-                <v-card-text>
-                  {{ $t('landing.players_where_text') }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-spacer cols="4"></v-spacer>
-          </v-row>
-          <v-row>
-            <v-spacer cols="2"></v-spacer>
-            <v-col cols="8">
-              <v-card>
-                <v-card-title>
-                  {{ $t('landing.players_who_title') }}
-                </v-card-title>
-                <v-card-text>
-                  {{ $t('landing.players_who_text') }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-spacer cols="2"></v-spacer>
-          </v-row>
-          <v-row>
-            <v-spacer cols="4"></v-spacer>
-            <v-col cols="8">
-              <v-card>
-                <v-card-title>
-                  {{ $t('landing.players_when_title') }}
-                </v-card-title>
-                <v-card-text>
-                  {{ $t('landing.players_when_text') }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
+    <v-container fluid class="bg-green-lighten-5" id="players">
+      <v-container>
+        <v-spacer class="my-10"></v-spacer>
+        <v-card elevation="0" class="bg-transparent">
+          <v-card-title class="text-center mb-5 text-h4 text-pre-wrap">
+            {{ $t('landing.players_title') }}
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="8">
+                <v-card>
+                  <v-card-title>
+                    {{ $t('landing.players_where_title') }}
+                  </v-card-title>
+                  <v-card-text>
+                    {{ $t('landing.players_where_text') }}
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-spacer cols="4"></v-spacer>
+            </v-row>
+            <v-row>
+              <v-spacer cols="2"></v-spacer>
+              <v-col cols="8">
+                <v-card>
+                  <v-card-title>
+                    {{ $t('landing.players_who_title') }}
+                  </v-card-title>
+                  <v-card-text>
+                    {{ $t('landing.players_who_text') }}
+                  </v-card-text>
+                </v-card>
+              </v-col>
+              <v-spacer cols="2"></v-spacer>
+            </v-row>
+            <v-row>
+              <v-spacer cols="4"></v-spacer>
+              <v-col cols="8">
+                <v-card>
+                  <v-card-title>
+                    {{ $t('landing.players_when_title') }}
+                  </v-card-title>
+                  <v-card-text>
+                    {{ $t('landing.players_when_text') }}
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+        <v-spacer class="my-10"></v-spacer>
+      </v-container>
     </v-container>
-    <v-spacer class="my-10"></v-spacer>
-    <v-container>
-      <v-card>
-        <v-card-title>
-          {{ $t('landing.clubs_title') }}
-        </v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col>
-              <v-card>
-                <v-card-text>
-                  {{ $t('landing.clubs_collection') }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card>
-                <v-card-text>
-                  {{ $t('landing.clubs_gatherings') }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col>
-              <v-card>
-                <v-card-text>
-                  {{ $t('landing.clubs_statistics') }}
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-row class="justify-center">
-            <v-col class="d-flex justify-center">
-              {{ $t('landing.clubs_bot') }}
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
+    <v-container fluid class="bg-orange-lighten-5" id="clubs">
+      <v-container>
+        <v-spacer class="my-10"></v-spacer>
+        <v-card elevation="0" class="bg-transparent">
+          <v-card-title class="text-center mb-5 text-h4 text-pre-wrap">
+            {{ $t('landing.clubs_title') }}
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col>
+                <v-card>
+                  <v-card-text>
+                    {{ $t('landing.clubs_collection') }}
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-card>
+                  <v-card-text>
+                    {{ $t('landing.clubs_gatherings') }}
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-card>
+                  <v-card-text>
+                    {{ $t('landing.clubs_statistics') }}
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row class="justify-center">
+              <v-col class="d-flex justify-center flex-column direction-column text-center align-center">
+                <div>
+                  {{ $t('landing.clubs_join_connect') }}
+                </div>
+                <v-btn color="blue" rounded variant="flat" class="ml-2" :href="'https://t.me/tirvy'" target="_blank">
+                  {{ $t('landing.clubs_join_link') }}
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-container>
+      <v-spacer class="my-10"></v-spacer>
     </v-container>
-    <v-footer>
+    <v-footer class="bg-grey">
       smth i dont know well
     </v-footer>
-  </v-container>
+  </v-main>
 </template>
 
 <script setup lang="ts">
@@ -143,6 +160,15 @@ watch(user, () => {
 definePageMeta({
   name: 'root-index',
 });
+
+const goTo = useGoTo();
+function scrollTo(id: string) {
+  goTo(id, { offset: -64 });
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.banner-container {
+  min-height: calc(100vh - 64px);
+}
+</style>
