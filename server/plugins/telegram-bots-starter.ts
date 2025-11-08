7005ed225b819e7b049f1ef25b72a53d8c5f1add
Ernest <8tirvy@gmail.com>
2025-11-08 22:41:04 +0400

    bots disable

--- server/plugins/telegram-bots-starter.ts
+++ server/plugins/telegram-bots-starter.ts
@@ -1,10 +1,10 @@
-import botBloodBowl from '~/server/telegram-bots/blood-bowl-tbilisi/blood-bowl';
+// import botBloodBowl from '~/server/telegram-bots/blood-bowl-tbilisi/blood-bowl';
 
-botBloodBowl
+// botBloodBowl
 
-import botFloodReader from '~/server/telegram-bots/flood-reader/flood-reader';
+// import botFloodReader from '~/server/telegram-bots/flood-reader/flood-reader';
 
-botFloodReader
+// botFloodReader
 
 import botLuba from '~/server/telegram-bots/luba-bot/luba-bot';
 
@@ -13,7 +13,7 @@ botLuba
 
 export default defineNitroPlugin((nitroApp) => {
     console.log("Checking bots launches...");
-    const bots = { botBloodBowl, botFloodReader, botLuba } as { [key: string]: any };
+    const bots = { botLuba } as { [key: string]: any };
     Object.keys(bots).forEach(botname => {
         if (bots[botname]) {
             console.log(`${botname} launch success\n`)
